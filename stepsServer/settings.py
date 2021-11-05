5946d0afc1923662d3249bdc47faeb1b54bee3ef
Kameron <lbpk17@yahoo.co.uk>
2021-11-05 16:22:38 +0000

    Added gunicorn logging

--- stepsServer/settings.py
+++ stepsServer/settings.py
@@ -180,3 +180,29 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
+if os.getenv('SERVER') == 'TRUE':
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': True,
+        'formatters': {
+            'verbose': {
+                'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s'
+            }
+        },
+        'handlers': {
+            'gunicorn': {
+                'level': 'DEBUG',
+                'class': 'logging.handlers.RotatingFileHandler',
+                'formatter': 'verbose',
+                'filename': '/opt/djangoprojects/reports/bin/gunicorn.errors',
+                'maxBytes': 1024 * 1024 * 100,  # 100 mb
+            }
+        },
+        'loggers': {
+            'gunicorn.errors': {
+                'level': 'DEBUG',
+                'handlers': ['gunicorn'],
+                'propagate': True,
+            },
+        }
+    }
