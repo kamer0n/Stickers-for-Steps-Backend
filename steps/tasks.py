0c5e2a21a593b5c4eb12c61efb8eaa2655e25ff9
Kameron <lbpk17@yahoo.co.uk>
2021-11-18 00:28:26 +0000

    Attempting to add cronjob

--- steps/tasks.py
+++ steps/tasks.py
@@ -10,7 +10,7 @@ app = Celery()
 def setup_periodic_tasks(sender, **kwargs):
 
     sender.add_periodic_task(
-        crontab(minute=27, hour=1),
+        crontab(minute=29, hour=1),
         clearStepsAndSticker.s(),
     )
 
