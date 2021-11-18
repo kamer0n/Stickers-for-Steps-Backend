ff388443142a02b939bed55073d2b2b4190c74f8
Kameron <lbpk17@yahoo.co.uk>
2021-11-18 00:26:53 +0000

    Attempting to add cronjob

--- steps/celery.py
+++ steps/celery.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
 
 app = Celery('steps')
 
--- steps/tasks.py
+++ steps/tasks.py
@@ -10,7 +10,7 @@ app = Celery()
 def setup_periodic_tasks(sender, **kwargs):
 
     sender.add_periodic_task(
-        crontab(minute=26, hour=1),
+        crontab(minute=27, hour=1),
         clearStepsAndSticker.s(),
     )
 
