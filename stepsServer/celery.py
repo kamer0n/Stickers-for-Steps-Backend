0fc82ac9fbe2d67c4dd0e8b865f8abbff57752f3
Kameron <lbpk17@yahoo.co.uk>
2021-11-18 01:16:56 +0000

    no idea

--- stepsServer/celery.py
+++ stepsServer/celery.py
@@ -10,7 +10,7 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 
 
-app = Celery('steps')
+app = Celery('stepsServer')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
@@ -26,7 +26,7 @@ app.conf.beat_schedule = {
     # Executes every day at  12:30 pm.
     'run-every-afternoon': {
         'task': 'steps.tasks.clearStepsAndSticker',
-        'schedule': crontab(hour=1, minute=16),
+        'schedule': crontab(hour=1, minute=18),
         'args': (),
     },
 }
