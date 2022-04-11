9af2cb349291f19d2ceb22b1da83cc78ff2379fb
Kameron <lbpk17@yahoo.co.uk>
2022-04-11 14:49:53 +0100

    celery

--- stepsServer/settings.py
+++ stepsServer/settings.py
@@ -20,6 +20,8 @@ from django.core.management.utils import get_random_secret_key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CELERY_RESULT_BACKEND = "redis"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
