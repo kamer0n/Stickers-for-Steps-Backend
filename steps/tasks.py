2f9c884ab938272683a0f55a2d5b7137d21f5302
Kameron <lbpk17@yahoo.co.uk>
2022-04-11 15:06:41 +0100

    celery

--- steps/tasks.py
+++ steps/tasks.py
@@ -15,9 +15,12 @@ def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(
         crontab(minute=15, hour=1),
         clearStepsAndSticker.s(),
-        bigTest.s(),
     )
 
+    sender.add_periodic_task(
+        crontab(minute=15, hour=1),
+        bigTest.s(),
+    )
 
 @shared_task
 def clearStepsAndSticker():
